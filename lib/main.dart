3309372827a6d5502e904ca7cd0710b8d86a9ba0
MK_22 <fred45722@gmail.com>
2024-02-04 12:44:26 +0000

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,94 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: "Clip Path",
+      debugShowCheckedModeBanner: false,
+      home: CurvedWidgets(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class CurvedWidgets extends StatefulWidget {
+  const CurvedWidgets({super.key});
 
+  @override
+  State<CurvedWidgets> createState() => _CurvedWidgets();
+}
+
+class _CurvedWidgets extends State<CurvedWidgets> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text('Customer Curves'),
+        backgroundColor: Colors.grey,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
+      body: curvedContainers(context),
+    );
+  }
+
+  Widget curvedContainers(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: ClipPath(
+              clipper: WaveClipper(),
+              child: Container(
+                color: Colors.red[200],
+                height: 200,
+              ),
+            ),
+          ),
+          ClipPath(
+            clipper: WaveClipper(),
+            child: Container(
+              color: Colors.red[500],
+              height: 180,
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    path.lineTo(0.0, size.height);
+
+    var firstStart = Offset(size.width / 5, size.height);
+
+    var firstEnd = Offset(size.width / 2.25, size.height - 50.0);
+
+    path.quadraticBezierTo(
+        firstStart.dx, firstStart.dy, firstEnd.dx, firstEnd.dy);
+
+    var secondStart =
+        Offset(size.width - (size.width / 3.24), size.height - 105);
+    var secondEnd = Offset(size.width, size.height - 10);
+
+    path.quadraticBezierTo(
+        secondStart.dx, secondStart.dy, secondEnd.dx, secondEnd.dy);
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper clipper) {
+    return true;
   }
 }
